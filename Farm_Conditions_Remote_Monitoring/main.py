e176a8922a5a923aabafe4dbaf9463ffe4287369
Ronnie-Leon76 <ronleon76@gmail.com>
2022-11-16 19:53:50 +0300

    farm_conditions_monitoring_system

--- Farm_Conditions_Remote_Monitoring/main.py
+++ Farm_Conditions_Remote_Monitoring/main.py
@@ -1,10 +1,25 @@
+"""
+Pivot Club KU: Hardware Cohort
+Farm Conditions Remote System using ESP32, Soil Moisture Sensor & DHT11
+Author: Ronnie Leon
+
+"""
+
+
+
 from machine import Pin, ADC
 from time import sleep
 import dht
 
+SOIL_WET = 500
+SOIL_DRY = 750
+
 # will change to the Analog Pin Connected
 analogue_pin = None
 moisture_sensor_pin = ADC(Pin(analogue_pin))
+
+# read voltage in full range by setting the attenuation ratio to 11db
+moisture_sensor_pin.atten(ADC.ATTN_11DB)
 
 # will change to the GPIO pin connected to DHT
 dht_pin = None
@@ -12,25 +27,10 @@ dht_pin = None
 dht_sensor = dht.DHT11(Pin(dht_pin))
 
 
-# read voltage in full range by setting the attenuation ratio to 11db
-moisture_sensor_pin.atten(ADC.ATTN_11DB)
 
-while True:
-    try:
-        moisture_value = moisture_sensor_pin.read()
-        print(moisture_value)
-        sleep(2)
-        moisture_sensor_pin.measure()
-        temp = moisture_sensor_pin.temperature()
-        hum = moisture_sensor_pin.humidity()
-        temp_f = temp * (9/5) + 32.0
-        print('Temperature: %3.1f C' %temp)
-        print('Temperature: %3.1f F' %temp_f)
-        print('Humidity: %3.1f %%' %hum)
-    except OSError as e:
-        print('Failed to read from the sensors.')
-        
-def web_page():
+
+
+def web_page(temp_c, temp_f, humid, moisture_status):
     html = """"""
     return html
 
@@ -38,19 +38,50 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
+
 while True:
-    conn, addr = s.accept()
-    print('Got a connection from %s' % str(addr))
-    request = conn.recv(1024)
-    request = str(request)
-    print('Content = %s' % request)
-    response = web_page()
-    conn.send('HTTP/1.1 200 OK\n')
-    conn.send('Content-Type: text/html\n')
-    conn.send('Connection: close\n\n')
-    conn.sendall(response)
-    conn.close()
+    try:
+        moisture_level = ""
+        moisture_value = moisture_sensor_pin.read()
+        if moisture_value < SOIL_WET:
+            print("Status: Soil is too wet")
+            moisture_level = "Too Wet"
+        elif moisture_value >= SOIL_WET and moisture_value < SOIL_DRY:
+            print("Status: Soil moisture is perfect")
+            moisture_level = "Perfect soil moisture"
+        elif moisture_value >= SOIL_DRY:
+            moisture_level = "Too Dry"
+            print("Status: Soil is too dry - time to water!")
+        sleep(2)
+        moisture_sensor_pin.measure()
+        temp_c = moisture_sensor_pin.temperature()
+        hum = moisture_sensor_pin.humidity()
+        temp_f = temp * (9/5) + 32.0
+        print('Temperature: %3.1f C' %temp_c)
+        print('Temperature: %3.1f F' %temp_f)
+        print('Humidity: %3.1f %%' %hum)
+        
+        
+        conn, addr = s.accept()
+        print('Got a connection from %s' % str(addr))
+        request = conn.recv(1024)
+        request = str(request)
+        print('Content = %s' % request)
+        response = web_page(temp_c, temp_f, hum, moisture_level)
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(response)
+        conn.close()
     
 
 
 
+
+
+    except OSError as e:
+        print('Failed to read from the sensors.')
+        
+
+
+while True
